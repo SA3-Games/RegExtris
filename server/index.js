925fdab57a326b3bf2e360d0daf7f8236bffcede
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-28 18:26:01 -0600

    fix: bug fix in server file

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
