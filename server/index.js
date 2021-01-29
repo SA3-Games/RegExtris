11a37450a328a9271b355642a2499d4a3fc8764b
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-29 15:26:41 -0600

    fix(server): path to send html file

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
