8c24f4f1c94b61f19280071538b3b161593d2756
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-29 15:19:00 -0600

    fix(server): dist folder changed to public

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
