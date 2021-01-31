3cb31dd297d599ff427899e4e74e1bed4e5654f3
Annie Cho <gjhwa11@gmail.com>
2021-01-31 14:49:24 -0500

    adds morgan middleware

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 app.listen(PORT, function () {
