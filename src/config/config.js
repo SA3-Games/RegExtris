ae674791134688aa500e5605ff08c93348a757d1
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-02-01 15:36:20 -0600

    feat(phaser): added empty rectangles for future score, regex, and next piece displays

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,17 @@
 export default {
   type: Phaser.AUTO,
-  width: 500,
-  height: 1000,
+  width: 1200,
+  height: 800,
   parent: 'game',
   render: {
     pixelArt: true,
   },
+  backgroundColor: 0xb7ede6,
   physics: {
     default: 'arcade',
     arcade: {
       gravity: { y: 0, x: 0 },
-      debug: true,
+      //debug: true,
     },
   },
 };
